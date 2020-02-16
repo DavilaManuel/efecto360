e05f7ed540ae3860ac49c25891b58c3b6683fb75
davilamanuel <manueldavila.developer@gmail.com>
2020-02-16 16:53:56 -0500

    details

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ window.addEventListener('load', pageLoaded)
 function pageLoaded() {
     for(let i=1; i<= 36; i++) {
         const number = i.toString().padStart(2, '00')
-        const url = `https://stockx-360.imgix.net/adidas-Yeezy-Boost-350-V2-Yecheil/Images/adidas-Yeezy-Boost-350-V2-Yecheil/Lv2/img${number}.jpg?auto=format,compress&w=559&q=90&dpr=2&updated_at=1574449122`
+        // const url = `https://stockx-360.imgix.net/adidas-Yeezy-Boost-350-V2-Yecheil/Images/adidas-Yeezy-Boost-350-V2-Yecheil/Lv2/img${number}.jpg?auto=format,compress&w=559&q=90&dpr=2&updated_at=1574449122`
+        const url = `https://stockx-360.imgix.net/Air-Jordan-11-Retro-Platinum-Tint-GS/Images/Air-Jordan-11-Retro-Platinum-Tint-GS/Lv2/img${number}.jpg?auto=format,compress&q=90&updated_at=1546585284&w=1000`
         const image = new Image()
         image.src = url
         image.addEventListener('load', () => {
